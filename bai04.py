8037f7fb5995d26e6b6af608e8dc7f2427b1ac3e
B24DTCN268-hoangvanquan <hoangvanquan1154@gmail.com>
2026-05-20 21:40:41 +0700

    Add files via upload

---
+++ bai04.py
@@ -0,0 +1,13 @@
+print("=== SỐ LƯỢNG NHÂN SỰ MỚI ===")
+
+# Khởi tạo biến số lượng nhân sự bằng giá trị không hợp lệ
+count = 0
+
+while count <= 0:
+    count = int(input("Vui lòng nhập số lượng nhân sự mới trong tháng này: "))
+
+    if count <= 0:
+        print("[LỖI] Số lượng phải là số nguyên dương (>0). Vui lòng nhập lại!\n")
+
+print(f"[THÀNH CÔNG] Đã ghi nhận yêu cầu cấp phát tài sản cho {count} nhân sự mới!")
+print("--- CHƯƠNG TRÌNH KẾT THÚC ---")
